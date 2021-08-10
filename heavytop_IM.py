503d43c2138599a6fc04edd9722918ab53b10fbf
Colin Cotter <cjc1@Colins-MacBook.local>
2021-08-10 14:10:22 +0100

    fixed some bugs with darryl

--- heavytop_IM.py
+++ heavytop_IM.py
@@ -10,13 +10,18 @@ Omega0 = Function(V)
 
 #problem setup########
 I0 = Identity(3)
-g = Constant(10.0)
-dt = 0.1
+g0 = 10.0
+g = Constant(g0)
+dt = 0.01
 dtc = Constant(dt)
-d = Constant(1.0)
-Chi.interpolate(as_vector([0,0,1]))
+d = Constant(4)
+Chi0 = [0, 1.0, 0]
+Chi.interpolate(as_vector([Chi0[0],
+                           Chi0[1],
+                           Chi0[2]]))
 Pi, Gamma = Un.split()
-Gamma.interpolate(as_vector([0.,1.0,0.0]))
+from math import sqrt
+Gamma.interpolate(as_vector([1.0/sqrt(3),1.0/sqrt(3),1.0/sqrt(3)]))
 Omega0.interpolate(as_vector([1.0,0.0,0.0]))
 Pi.interpolate(dot(I0, Omega0) + d**2/Constant(12.0)*(
     Omega0 - inner(Gamma, Omega0)*Gamma))
@@ -29,7 +34,7 @@ Pinh = Constant(0.5)*Pin + Constant(0.5)*Pinp
 Gammanh = Constant(0.5)*Gamman + Constant(0.5)*Gammanp
 
 I = I0 + d**2/Constant(12.0)*(Identity(3)*inner(Gammanh, Gammanh)
-                              - outer(Gammanh, Gammanh)) 
+                              - outer(Gammanh, Gammanh))
 Iinv = inv(I)
 
 Omeganh = dot(Iinv, Pinh)
@@ -42,10 +47,9 @@ eq_vec = g*(Gammanh - Chi) \
 
 eqn = (
     inner(Pinp - Pin + dtc*cross(Pinh, Omeganh)
-          + dtc*cross(Pinh, eq_vec), dPi)
-    +
-    inner(Gammanp - Gamman + dtc*cross(Gammanh, eq_vec), dGamma)
-    )*dx
+          + dtc*cross(Gammanh, eq_vec), dPi)+
+    inner(Gammanp - Gamman + dtc*cross(Gammanh, Omeganh), dGamma)
+)*dx
 
 solver_parameters = {
     'ksp_type': 'preonly',
@@ -55,7 +59,19 @@ solver_parameters = {
 t_prob = NonlinearVariationalProblem(eqn, Unp)
 t_solver = NonlinearVariationalSolver(t_prob, solver_parameters=
                                      solver_parameters)
-    
+
+#Computing Omega
+Omegan = Function(V)
+Omega = TrialFunction(V)
+dOmega = TestFunction(V)
+I = I0 + d**2/Constant(12.0)*(Identity(3)*inner(Gamman, Gamman)
+                              - outer(Gamman, Gamman))
+
+eqn = inner(dOmega, dot(I, Omega) - Pin)*dx
+o_prob = LinearVariationalProblem(lhs(eqn), rhs(eqn), Omegan)
+o_solver = LinearVariationalSolver(o_prob, solver_parameters=
+                                   solver_parameters)
+
 tmax = 1.0
 t= 0.0
 Unp.assign(Un)
@@ -63,14 +79,78 @@ nct = 0
 import numpy as np
 npts = int(np.ceil(tmax/dt)+1)
 Gammas = np.zeros((3, npts))
+Omegas = np.zeros((3, npts))
 Pis = np.zeros((3, npts))
 Pi, Gamma = Un.split()
+ts = np.zeros(npts)
+Pis[:, 0] = Pi.dat.data
+Gammas[:,0] = Gamma.dat.data
+o_solver.solve()
+Omegas[:, 0] = Omegan.dat.data
 while t < tmax - 0.5*dt:
     t += dt
-    print(t)
     t_solver.solve()
     Un.assign(Unp)
+    o_solver.solve()
     nct += 1
-
     Pis[:, nct] = Pi.dat.data
     Gammas[:, nct] = Gamma.dat.data
+    Omegas[:, nct] = Omegan.dat.data
+    ts[nct] = t
+
+import matplotlib.pyplot as pp
+
+gtimeplots = True
+if gtimeplots:
+    pp.subplot(3,1,1)
+    pp.plot(ts, Gammas[0, :])
+    pp.subplot(3,1,2)
+    pp.plot(ts, Gammas[1, :])
+    pp.subplot(3,1,3)
+    pp.plot(ts, Gammas[2, :])
+    pp.title("Gamma")
+    pp.show()
+
+ptimeplots = True
+if ptimeplots:
+    pp.subplot(3,1,1)
+    pp.plot(ts, Pis[0, :])
+    pp.subplot(3,1,2)
+    pp.plot(ts, Pis[1, :])
+    pp.subplot(3,1,3)
+    pp.plot(ts, Pis[2, :])
+    pp.title("Pi")
+    pp.show()
+
+otimeplots = True
+if otimeplots:
+    pp.subplot(3,1,1)
+    pp.plot(ts, Omegas[0, :])
+    pp.subplot(3,1,2)
+    pp.plot(ts, Omegas[1, :])
+    pp.subplot(3,1,3)
+    pp.plot(ts, Omegas[2, :])
+    pp.title("Omega")
+    pp.show()
+
+diags = True
+if diags:
+    pp.subplot(3,1,1)
+    pp.plot(ts, (np.sum(Gammas**2, axis=0))**0.5-1)
+    pp.subplot(3,1,2)
+    casimir = np.sum(Gammas*Pis, axis=0)
+    pp.plot(ts, casimir - casimir[0])
+    pp.subplot(3,1,3)
+    Energy = 0.5*np.sum(Omegas*Pis, axis=0) + \
+        0.5*g0*((Gammas[0, :] - Chi0[0])**2 +
+               (Gammas[1, :] - Chi0[1])**2 +
+               (Gammas[2, :] - Chi0[2])**2)
+    print(Energy.shape, ts.shape)
+    pp.plot(ts, Energy - Energy[0])
+    pp.show()
+
+locus = True
+if locus:
+    pp.plot(Gammas[0, :], Gammas[1, :])
+    pp.plot(Gammas[0, -1], Gammas[1, -1], 'ro')
+    pp.show()
